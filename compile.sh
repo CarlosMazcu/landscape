2cf59759898d252e8adc670cf8d093dd81322bca
CarlosMazcu <mazcunyanbla@esat-alumni.com>
2025-11-04 11:18:46 +0100

    fix compile obj/ dir

--- compile.sh
+++ compile.sh
@@ -1,11 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Crear el directorio obj/ si no existe
+mkdir -p obj
 
 # Para compilar en linux x86/x64
- gcc -c -g -Wall -O2 -o obj/landscape.o landscape.c
+gcc -c -g -Wall -O2 -o obj/landscape.o landscape.c
+
 # Para compilar ARM
-# gcc -c -g -Wall -mfpu=vfp -O2 -o obj/dot_canvas.o dot_canvas.c
+# gcc -c -g -Wall -mfpu=vfp -O2 -o obj/landscape.o landscape.c
 
 gcc -c -g -Wall -O2 -o obj/chrono.o chrono.c
 
 cd obj
-
-gcc -o ../landscape.elf landscape.o chrono.o  -lm -lSDL 
+gcc -o ../landscape.elf landscape.o chrono.o -lm -lSDL
